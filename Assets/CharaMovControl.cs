aa65a5587b475b517e1e4746ece041848921ba12
mirz1911 <45736800+mirz1911@users.noreply.github.com>
2022-06-12 18:22:03 +0700

    Control + Physics
    for chara of course

---
+++ Assets/CharaMovControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaMovControl : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 velocityVector = rig.velocity;
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+        rig.velocity = velocityVector;
+    }
+}
